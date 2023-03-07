acca6de06f18f74cf277dd2fb24f9fb74ae90975
Charles <charleslynchj@outlook.com>
2023-03-07 23:11:48 +1030

    -

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include "Referee.h"
 
 #include <iostream>
-#include <memory>
+#include <memory> //for smart pointer objects using 'unique_ptr'
 
 int main() {
-    //dynamic allocation of memory for pointer objects (pointing to player instance)
+    //dynamic allocation of memory using smart pointer objects (pointing to player instance)
     std::unique_ptr<Player> computer(new Computer());
     std::unique_ptr<Player> human(new Human());
     //ref is an instance of Referee
